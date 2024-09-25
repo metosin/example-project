925ccf7bf035d2175d784301f0e0b8aefc38a917
Juho Teperi <juho.teperi@iki.fi>
2024-09-25 10:52:04 +0300

    Add MUI

--- frontend/src/app/core.cljs
+++ frontend/src/app/core.cljs
@@ -1,5 +1,8 @@
 (ns app.core
-  (:require [cljs.spec.alpha :as s]
+  (:require ["@mui/material/Button$default" :as Button]
+            ["@mui/material/Checkbox$default" :as Checkbox]
+            ["@mui/material/Stack$default" :as Stack]
+            ["@mui/material/Typography$default" :as Typography]
             [uix.core :as uix :refer [defui $]]
             [uix.dom]
             [app.hooks :as hooks]
@@ -11,83 +14,80 @@
 
 (defui header []
   ($ :header.app-header
-    ($ :img {:src "https://raw.githubusercontent.com/pitch-io/uix/master/logo.png"
-             :width 32})))
+     ($ :img {:src "https://raw.githubusercontent.com/pitch-io/uix/master/logo.png"
+              :width 32})))
 
 (defui footer []
   ($ :footer.app-footer
-    ($ :small "made with "
-              ($ :a {:href "https://github.com/pitch-io/uix"}
-                    "UIx"))))
+     ($ :small "made with "
+        ($ :a {:href "https://github.com/pitch-io/uix"}
+           "UIx"))))
 
 (defui text-field [{:keys [on-add-todo]}]
   (let [[value set-value!] (uix/use-state "")]
     ($ :input.text-input
-      {:value value
-       :placeholder "Add a new todo and hit Enter to save"
-       :on-change (fn [^js e]
-                    (set-value! (.. e -target -value)))
-       :on-key-down (fn [^js e]
-                      (when (= "Enter" (.-key e))
-                        (set-value! "")
-                        (on-add-todo {:text value :status :unresolved})))})))
+       {:value value
+        :placeholder "Add a new todo and hit Enter to save"
+        :on-change (fn [^js e]
+                     (set-value! (.. e -target -value)))
+        :on-key-down (fn [^js e]
+                       (when (= "Enter" (.-key e))
+                         (set-value! "")
+                         (on-add-todo {:text value :status :unresolved})))})))
 
 (defui editable-text [{:keys [text text-style on-done-editing]}]
   (let [[editing? set-editing!] (uix/use-state false)
         [editing-value set-editing-value!] (uix/use-state "")]
     (if editing?
       ($ :input.todo-item-text-field
-        {:value editing-value
-         :auto-focus true
-         :on-change (fn [^js e]
-                      (set-editing-value! (.. e -target -value)))
-         :on-key-down (fn [^js e]
-                        (when (= "Enter" (.-key e))
-                          (set-editing-value! "")
-                          (set-editing! false)
-                          (on-done-editing editing-value)))})
+         {:value editing-value
+          :auto-focus true
+          :on-change (fn [^js e]
+                       (set-editing-value! (.. e -target -value)))
+          :on-key-down (fn [^js e]
+                         (when (= "Enter" (.-key e))
+                           (set-editing-value! "")
+                           (set-editing! false)
+                           (on-done-editing editing-value)))})
       ($ :span.todo-item-text
-        {:style text-style
-         :on-click (fn [_]
-                     (set-editing! true)
-                     (set-editing-value! text))}
-        text))))
-
-(s/def :todo/text string?)
-(s/def :todo/status #{:unresolved :resolved})
-
-(s/def :todo/item
-  (s/keys :req-un [:todo/text :todo/status]))
+         {:style text-style
+          :on-click (fn [_]
+                      (set-editing! true)
+                      (set-editing-value! text))}
+         text))))
 
 (defui todo-item
-  [{:keys [created-at text status on-remove-todo on-set-todo-text] :as props}]
-  {:pre [(s/valid? :todo/item props)]}
-  ($ :.todo-item
-    {:key created-at}
-    ($ :input.todo-item-control
-      {:type :checkbox
-       :checked (= status :resolved)
-       :on-change #(rf/dispatch [:todo/toggle-status created-at])})
-    ($ editable-text
-      {:text text
-       :text-style {:text-decoration (when (= :resolved status) :line-through)}
-       :on-done-editing #(on-set-todo-text created-at %)})
-    ($ :button.todo-item-delete-button
-      {:on-click #(on-remove-todo created-at)}
-      "×")))
+  [{:keys [created-at text status on-remove-todo on-set-todo-text]}]
+  ($ Stack
+     {:direction "row"
+      :sx #js {:my 1}}
+     ($ Checkbox
+        {:checked (= status :resolved)
+         :on-change #(rf/dispatch [:todo/toggle-status created-at])})
+     ($ editable-text
+        {:text text
+         :text-style {:text-decoration (when (= :resolved status) :line-through)}
+         :on-done-editing #(on-set-todo-text created-at %)})
+     ($ Button
+        {:on-click #(on-remove-todo created-at)
+         ;;               v- theme color
+         :sx #js {:color "error.main"}}
+        "×")))
 
 (defui app []
   (let [todos (hooks/use-subscribe [:app/todos])]
-    ($ :.app
-      ($ header)
-      ($ text-field {:on-add-todo #(rf/dispatch [:todo/add %])})
-      (for [[created-at todo] todos]
-        ($ todo-item
-          (assoc todo :created-at created-at
-                      :key created-at
-                      :on-remove-todo #(rf/dispatch [:todo/remove %])
-                      :on-set-todo-text #(rf/dispatch [:todo/set-text %1 %2]))))
-      ($ footer))))
+    ($ Stack
+       {}
+       ($ header)
+       ($ text-field {:on-add-todo #(rf/dispatch [:todo/add %])})
+       (for [[created-at todo] todos]
+         ($ todo-item
+            (assoc todo
+                   :created-at created-at
+                   :key created-at
+                   :on-remove-todo #(rf/dispatch [:todo/remove %])
+                   :on-set-todo-text #(rf/dispatch [:todo/set-text %1 %2]))))
+       ($ footer))))
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
